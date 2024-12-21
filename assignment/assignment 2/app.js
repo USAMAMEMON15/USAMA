3c0fc8ddf1e3288c8a7400c9fe1490e76e206626
usama <usama15memon@gmail.com>
2024-12-21 16:50:22 +0500

    assignment

--- assignment/assignment 2/app.js
+++ assignment/assignment 2/app.js
@@ -1,3 +1,4 @@
+
 // init Isotope
 var $grid = $('.container').isotope({
     // options
@@ -11,3 +12,35 @@ var $grid = $('.container').isotope({
   $(document).on("click" ,"ul li",function(){
     $(this).addClass("active").siblings().removeClass("active")
   })
+
+  function velidation(){
+    let email =document.getElementById("email").value;
+    let password =document.getElementById("password").value;
+
+    let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let passwordCheck = /^[A-Za-z]\w{5,12}$/;
+     
+   
+    if(emailCheck.test(email)){
+        document.getElementById("emailerror").innerHTML = ""
+    }
+    else{
+        
+        document.getElementById("emailerror").innerHTML = "***invalid email"
+        return false;
+}
+
+    if(passwordCheck.test(password)){
+        document.getElementById("passworderror").innerHTML = ""
+    }
+    else{
+        document.getElementById("passworderror").innerHTML = "***At leat min 5 characters"
+        return false;
+    }
+
+   
+if (emailCheck.test(email) && passwordCheck.test(password)){
+    swal("Form Submit Successful", "Waiting for the email", "success")
+    return false;
+}
+}
